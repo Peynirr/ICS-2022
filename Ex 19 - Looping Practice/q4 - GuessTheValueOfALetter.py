468d76367885f117420c1b63ec9e4539f831ed6e
Peynirr <omercan7045@gmail.com>
2022-04-03 19:16:14 -0400

    updates & polished

--- Ex 19 - Looping Practice/q4 - GuessTheValueOfALetter.py
+++ Ex 19 - Looping Practice/q4 - GuessTheValueOfALetter.py
@@ -3,3 +3,24 @@
 # Ex 19 Q4
 # Asking the User to Guess the Value of a Letter from A to Z
 
+letter = "O"
+
+#input
+guess = str(input("Please enter a CAPITAL single letter of your choice from A to Z:\t"))
+
+#process
+while guess <= str(9):
+    guess = str(input("Invalid. Please enter a single letter:\t"))
+
+else:
+    while guess != letter:
+        if guess > letter:
+            print("You are wrong; go closer to A.")
+            guess = str(input("Try again. Choose a single letter of your choice from A to Z:\t"))
+
+        elif guess < letter:
+            print("You are wrong; go close to Z.")
+            guess = str(input("Try again. Choose a single letter of your choice from A to Z:\t"))
+            
+    else:
+        print("You are correct. Congratulations!")
