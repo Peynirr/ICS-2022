3a8375b4c26bcbd7f74839ea58cc1d6845883ab7
Omercan Ozkan <omercan7045@gmail.com>
2022-04-04 13:22:06 -0400

    updated & added

---
+++ Practice While Loops/q1 - FindingNumbersDivisible3.py
@@ -0,0 +1,13 @@
+# Omercan
+# April 4 2022
+# Practice While Loops - Q1
+# Finding numbers that are divisible by 3
+
+x = 100
+
+while x <= 100:
+    x += 100
+    n = x
+    if n % 3:
+        print(n)
+
