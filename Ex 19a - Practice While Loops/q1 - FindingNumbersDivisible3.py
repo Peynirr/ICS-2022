796565374aa342e861fb57cc2aebdc4eec769cca
Omercan Ozkan <omercan7045@gmail.com>
2022-04-05 14:45:59 -0400

    updated

--- Ex 19a - Practice While Loops/q1 - FindingNumbersDivisible3.py
+++ Ex 19a - Practice While Loops/q1 - FindingNumbersDivisible3.py
@@ -3,9 +3,5 @@
 # Practice While Loops - Q1
 # Finding numbers that are divisible by 3
 
-x = 100
 
-while x <= 100:
-    x % 3
-    print(x)
 
--- Ex 19a - Practice While Loops/q2 - FindingNumbersMultiple5.py
+++ Ex 19a - Practice While Loops/q2 - FindingNumbersMultiple5.py
@@ -2,9 +2,3 @@
 # April 4 2022
 # Practice While Loops - Q2
 # Finding numbers that are a multiple of 5
- 
-x = 200
- 
-while x <= 200:
-   x % 3
-   print(x)
