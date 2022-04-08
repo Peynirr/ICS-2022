541762d50d9077c2a662a8cae597c523c85885aa
Peynirr <88389659+Peynirr@users.noreply.github.com>
2022-04-07 22:04:24 -0400

    newly added

---
+++ Ex 22 - Random Values/q2 - AssignTheVariableNum.py
@@ -0,0 +1,12 @@
+# Omercan
+# April 7 2022
+# Ex 22 Q2
+# Assigning a Variable to a Random Value From a Set of Integers
+
+import random
+
+#Process
+for i in range (21):
+    num = random.randrange(100, 425, 25)
+    variation = num / 100
+    print(variation)
---
+++ Ex 22 - Random Values/q3 - CoinFlipperRandomNumber.py
@@ -0,0 +1,5 @@
+# Omercan
+# April 7 2022
+# Ex 22 Q3
+# Displaying and Randomly Flipping a Coin; Heads or Tails
+
