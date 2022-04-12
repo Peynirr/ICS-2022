1e01e4e3a37ce3a8691e9a060d08875283e19a7f
Omercan Ozkan <omercan.ozkan@student.tdsb.on.ca>
2022-04-12 19:52:56 -0400

    updated

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ count = 2
 while(count < 40): 
     if count % 10 == 0: 
         print()
-    print("{:5}{:5}".format(count, count**2))
+    print("{:5}{:5}".format(count, count ** 2))
     count = count + 2
