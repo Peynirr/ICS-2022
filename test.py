c435efa996bf38fe3225080ca2fac400d53f8df0
Peynirr <88389659+Peynirr@users.noreply.github.com>
2022-04-27 14:54:26 -0400

    first commit

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-one = [1, 2, 3, 4, 6, 7, 8, 9, 10]
-
-for i in range(len(one)):
-    if i % 2 == 1:
-        print(one[i])
-
-
+fileNew = open("sample.txt",'w')
+fileRead = open("sample.txt", 'r')
+fileAdd = open("sample.txt",'a')
+hamlet = ["To", "be", "or", "not", "to", "be", "that", "is", "the", "question"]
+for i in hamlet:
+    fileAdd.write(str(i) + "\n")
+    fileNew.close()
+fileRead.readlines
+fileNew.close()
