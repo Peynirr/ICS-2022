e7fff92c6b95683d2f4c267474302c536594bc10
Omercan Ozkan <omercan.ozkan@student.tdsb.on.ca>
2022-04-28 23:53:24 -0400

    newly added

---
+++ Review/Test #3 - Lists, Files, and Functions/q6 - CreatesFileWritesWordsFromList.py
@@ -0,0 +1,17 @@
+# Omercan
+# April 28 2022
+# Review - Q6
+# Creating a File and Adding Text from a List to it
+
+#List
+foobar = ["toronto", "vancouver", "montreal", "calgary", "halifax"]
+
+#Process
+fileWrite = open("review.txt", 'w') #Creates the file
+fileAppend = open("review.txt", 'a') #Adds to the file
+
+for element in foobar: #For Loop
+    fileWrite.write(str(element) + "\n") #Writes then skips a line for every element in the list
+    fileAppend.write(element + "\n")
+
+fileWrite.close() #Closes the file
