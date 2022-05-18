87f79fc099d222a93f27b1ef4d2b2c37028bb38e
Omercan Ozkan <omercan.ozkan@student.tdsb.on.ca>
2022-05-18 17:57:16 -0400

    modifed

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
 # Test Environment
 
-stuff = [-5, 34, 18, 9]
-thing = [0.1,0.5, 7.25, 14.20, -3.0, -6.8]
-foobar = ["toronto", "vancouver", "montreal", "calgary", "halifax" , "St. John’s"]
-yolo = [1, True, 15.5, "Nay", 10, 20, 15, 40, 50]
-
-
-
-
