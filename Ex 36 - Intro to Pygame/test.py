16b5ee0a4f45029c3f6b0bca18e1d8ded24e6522
Peynirr <88389659+Peynirr@users.noreply.github.com>
2022-05-19 17:15:47 -0400

    first commit

--- Ex 36 - Intro to Pygame/test.py
+++ Ex 36 - Intro to Pygame/test.py
@@ -16,17 +16,17 @@ pygame.display.set_caption("Squares")
 screen.fill(THECOLORS["ivory"])
 
 #Rectangles
-pygame.draw.rect(screen, (THECOLORS["cornflowerblue"]), (0, 0, 100, 100))
-pygame.draw.rect(screen, (THECOLORS["seagreen2"]), (600, 0, 100, 100))
-pygame.draw.rect(screen, (THECOLORS["orangered1"]), (0, 500, 100, 100))
-pygame.draw.rect(screen, (THECOLORS["darkorchid2"]), (600, 500, 100, 100))
+pygame.draw.circle(screen, (THECOLORS["cornflowerblue"]), (100, 500), 100)
+pygame.draw.circle(screen, (THECOLORS["seagreen2"]), (600, 100), 100)
+pygame.draw.circle(screen, (THECOLORS["orangered1"]), (100, 100), 100)
+pygame.draw.circle(screen, (THECOLORS["darkorchid2"]), (600, 500,), 100)
 
 #Circle
 pygame.draw.circle(screen, (THECOLORS["yellow1"]),  (350, 300), 100)
 
 #Lines
-pygame.draw.line(screen, (THECOLORS["maroon1"]), (0, 300), (700, 300), 4)
-pygame.draw.line(screen, (THECOLORS["deeppink1"]), (350, 0), (350, 600), 4)
+pygame.draw.line(screen, (THECOLORS["maroon1"]), (0, 600), (700, 0), 4)
+pygame.draw.line(screen, (THECOLORS["deeppink1"]), (700, 0), (350, 600), 4)
 
 pygame.display.flip()
 
@@ -41,3 +41,4 @@ while keepGoing:
             keepGoing = False
 pygame.display.flip()
 pygame.quit()
+
